a2c583512713188a85748ab1c9e444d4de977b04
Lixel Amos <amosbett99@gmail.com>
2023-03-14 14:33:06 +0300

    completed assignments on arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;  // multiply = 62
+
+let random = Math.floor(Math.random() * 100) + 1;  // generates a random integer between 1 and 100
+console.log("Random number: " + random);
+
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;  // mod = 4
+
+let numbers = [7, 20, 12, 6, 8];
+let max= Math.max(7,20,12,6,8);  // max_num = 20
